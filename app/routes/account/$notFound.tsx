f34904dd51710ec4b01ca0c3a4ee913c18c80333
Jamee Kim <jameekim@nodaplife.me>
2022-04-08 18:43:56 -0400

    Add route for collections

---
+++ app/routes/account/$notFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(): JSX.Element {
+  return (
+    <>
+      <h3>Page Not Found</h3>
+      <p>Please select one of the tabs above.</p>
+    </>
+  );
+}
---
+++ app/routes/account/collections.tsx
@@ -0,0 +1,15 @@
+import { ActionFunction, LoaderFunction } from "remix";
+
+export const loader: LoaderFunction = async () => {
+  // TODO
+  return null;
+};
+
+export const action: ActionFunction = async () => {
+  // TODO
+  return null;
+};
+
+export default function Collections(): JSX.Element {
+  return <div></div>;
+}
